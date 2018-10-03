463906bef5373c233f54267c2faf637873ed4418
S Harish <sharish@akamai.com>
2018-10-03 21:56:39 +0530

    FizzBuzzConverter with multiples of three
    Added code for checking divisibilty of three

--- src/FizzBuzzConverter.java
+++ src/FizzBuzzConverter.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class FizzBuzzConverter {
+
+    public String convert(int number) {
+        if(number%3 == 0){
+            return "Fizz";
+        }
+        return String.valueOf(number);
+    }
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class FizzBuzzTest {
+    @Test
+    public void fizzBuzzConverterForNormalNumbers(){
+        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+        Assert.assertEquals("1",fizzBuzzConverter.convert(1));
+    }
+
+    @Test
+    public void FizzBuzzConverterForMultiplesOfThree(){
+        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+        Assert.assertEquals("Fizz",fizzBuzzConverter.convert(3));
+    }
 }
